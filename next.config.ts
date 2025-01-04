cecfa68246263360397f60f97982420dee5e3d6b
Iftee97 <tawsifhaque97@gmail.com>
2025-01-04 16:32:25 +0600

    some fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**", // Adjust the pathname if necessary to be more specific
+        search: "", // Leave this empty unless you need to match specific query parameters
+      },
+    ],
+  },
 };
 
 export default nextConfig;
